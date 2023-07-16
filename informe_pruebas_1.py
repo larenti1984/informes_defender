f97f8f381a6e44da8c8e8fea77be416348e6d9f0
Matias Larenti <matias.larenti@gmail.com>
2023-07-16 10:26:41 -0300

    se agrego el menu de resumen y confirmacion de envio de correos

--- informe_pruebas_1.py
+++ informe_pruebas_1.py
@@ -3,41 +3,95 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from tabulate import tabulate
+import os
 
-# Leer la planilla de Excel
-df = pd.read_excel('C:/Proyectos/infDef_python/informes_defender/informe_modificado.xlsx')
+# Ruta del archivo Excel
+ruta_archivo = 'C:/Proyectos/infDef_python/informes_defender/informe_modificado.xlsx'
+
+# Verificar si el archivo existe y está cerrado
+if not os.path.exists(ruta_archivo):
+    print(f"El archivo {ruta_archivo} no existe.")
+    # Puedes agregar aquí el código para manejar esta situación de error
+    # Por ejemplo, puedes lanzar una excepción o enviar una notificación.
+
+try:
+    df = pd.read_excel(ruta_archivo)
+except PermissionError:
+    print(f"El archivo {ruta_archivo} está abierto. Cierre el archivo y vuelva a intentarlo.")
+    # Puedes agregar aquí el código para manejar esta situación de error
+    # Por ejemplo, puedes lanzar una excepción o enviar una notificación.
 
 # Agrupar los datos por correo y generar el cuerpo del correo
 grupo_correo = df.groupby('correo')
+resumen = {}
+
 for correo, grupo in grupo_correo:
     usuario = grupo.iloc[0]['usuario']
-    datos_tabla = grupo[['VulnerabilitySeverityLevel', 'RegistryDeviceTag', 'RecommendedSecurityUpdate']]
-    tabla = tabulate(datos_tabla, headers=['Urgencia', 'Aplicacion', 'Recomendacion'], tablefmt='orgtbl')
+    cantidad_items = len(grupo)
 
-    # Construir el cuerpo del correo
-    cuerpo_correo = f"Estimado {usuario}, detectamos que su equipo tiene los siguientes problemas de updates:\n\n{tabla}"
+    # Actualizar el resumen
+    resumen[correo] = {
+        'Usuario': usuario,
+        'Cantidad de items': cantidad_items
+    }
 
-    # Configurar los detalles del correo saliente
-    remitente = 'tu_correo@tudominio.com'  # Cambia esto por tu dirección de correo electrónico
-    password = 'tu_contraseña'  # Cambia esto por tu contraseña de correo electrónico
-    servidor_smtp = 'smtp.office365.com'  # Servidor SMTP de Office 365
-    puerto_smtp = 587  # Puerto SMTP de Office 365
+# Mostrar resumen de correos
+print(f"Se enviarán {len(resumen)} correos:")
+for correo, info in resumen.items():
+    print(f"- Correo: {correo}")
+    print(f"  Usuario: {info['Usuario']}")
+    print(f"  Cantidad de items: {info['Cantidad de items']}")
 
-    destinatario = correo
+# Preguntar si se desea ver el resumen detallado
+ver_resumen_detallado = input("¿Deseas ver el resumen detallado? (y/n): ")
 
-    # Crear el objeto MIMEMultipart para el correo
-    mensaje = MIMEMultipart()
-    mensaje['From'] = remitente
-    mensaje['To'] = destinatario
-    mensaje['Subject'] = 'Problemas de updates en tu equipo'
+if ver_resumen_detallado.lower() == 'y':
+    for correo, info in resumen.items():
+        usuario = info['Usuario']
+        cantidad_items = info['Cantidad de items']
 
-    # Agregar el cuerpo del correo al objeto MIMEText
-    mensaje.attach(MIMEText(cuerpo_correo, 'plain'))
+        datos_tabla = grupo[grupo['correo'] == correo][['VulnerabilitySeverityLevel', 'SoftwareName', 'RecommendedSecurityUpdate']]
+        tabla = tabulate(datos_tabla, headers=['Urgencia', 'Aplicacion', 'Recomendacion'], tablefmt='orgtbl')
 
-    # Enviar el correo utilizando el servidor SMTP de Office 365
-    with smtplib.SMTP(servidor_smtp, puerto_smtp) as servidor:
-        servidor.starttls()
-        servidor.login(remitente, password)
-        servidor.send_message(mensaje)
+        print(f"\nDetalles para el correo: {correo}")
+        print(f"Usuario: {usuario}")
+        print(f"Cantidad de items: {cantidad_items}")
+        print(f"Tabla de problemas:\n{tabla}\n")
 
-    print(f"Correo enviado a: {destinatario}")
+# Confirmar el envío de los correos
+confirmacion = input("¿Deseas enviar los correos? (y/n): ")
+
+if confirmacion.lower() == 'y':
+    # Enviar los correos
+    servidor_smtp = 'smtp.gmail.com'  # Cambia esto según tu proveedor de correo
+    puerto_smtp = 587  # Cambia esto según tu proveedor de correo
+    remitente = 'neorisp@gmail.com'  # Cambia esto por tu dirección de correo electrónico
+    password = 'hrwzhgivekxmibir'  # Cambia esto por tu contraseña de correo electrónico
+
+    for correo, info in resumen.items():
+        destinatario = correo
+
+        # Construir el cuerpo del correo
+        usuario = info['Usuario']
+        cuerpo_correo = f"Estimado {usuario}, detectamos que su equipo tiene los siguientes problemas de updates:\n\n{tabla}"
+
+        # Crear el objeto MIMEMultipart para el correo
+        mensaje = MIMEMultipart()
+        mensaje['From'] = remitente
+        mensaje['To'] = destinatario
+        mensaje['Subject'] = 'Problemas de updates en tu equipo'
+
+        # Agregar el cuerpo del correo al objeto MIMEText
+        mensaje.attach(MIMEText(cuerpo_correo, 'plain'))
+
+        # Enviar el correo utilizando el servidor SMTP
+        with smtplib.SMTP(servidor_smtp, puerto_smtp) as servidor:
+            servidor.starttls()
+            servidor.login(remitente, password)
+            servidor.send_message(mensaje)
+
+        print(f"Correo enviado a: {destinatario}")
+
+else:
+    print("No se enviaron los correos.")
+
